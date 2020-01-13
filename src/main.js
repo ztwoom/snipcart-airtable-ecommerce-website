dbabc84195b77fd94a41e49f287683990ab06a23
Ahsath <ahsath@gmail.com>
2020-01-12 22:24:25 -0500

    Adding snicart inline style

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import "../src/assets/tailwind.css";
 import "typeface-roboto";
-import "../src/assets/styles.css";
 import AppShell from "./layouts/AppShell";
 import ProductDetailList from "./components/ProductDetailList";
 import ProductDetailItem from "./components/ProductDetailItem";
 import AppHover from "./components/AppHover";
 import AppModal from "./components/AppModal";
+import Message from "vue-m-message";
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
+  head.style.push({
+    type: "text/css",
+    cssText: `
+    .snipcart-modal__container {
+      background-color: #F5F5F5 !important;
+      z-index: 30 !important;
+    }
+    `
+  });
+  Vue.use(Message);
   Vue.component("AppShell", AppShell);
   Vue.component("ProductDetailList", ProductDetailList);
   Vue.component("ProductDetailItem", ProductDetailItem);
